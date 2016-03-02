9fffe1d500289ffa52e6eb5e76b3a51dd520484e
Babygogogo <101599838@qq.com>
2016-03-03 01:26:37 +0800

    Update main.lua to load the new SceneMain and remove the old one.

--- src/main.lua
+++ src/main.lua
@@ -7,7 +7,18 @@ require "config"
 require "cocos.init"
 
 local function main()
-    require("app.MyApp"):create():run("SceneMain")
+--    require("app.MyApp"):create():run("SceneMain")
+---[[
+    display.loadSpriteFrames("BabyWarsTextureTile.plist", "BabyWarsTextureTile.png")
+    display.loadSpriteFrames("BabyWarsTextureUnit.plist", "BabyWarsTextureUnit.png")
+    display.loadSpriteFrames("BabyWarsTextureUI.plist",   "BabyWarsTextureUI.png")
+    
+    require("app.utilities.AnimationLoader").load()
+    
+    math.randomseed(os.time())
+        
+    display.runScene(require("app.scenes.SceneMain"):create())
+--]]
 end
 
 local status, msg = xpcall(main, __G__TRACKBACK__)
