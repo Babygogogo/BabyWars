3823a774b8e6b0a642a7c4129f94ea7e1290d015
Babygogogo <101599838@qq.com>
2016-03-23 14:21:36 +0800

    The move path for units can now be calculated correctly.

--- src/app/models/ModelActionPlanner.lua
+++ src/app/models/ModelActionPlanner.lua
@@ -4,27 +4,10 @@ local ModelActionPlanner = class("ModelActionPlanner")
 local GridIndexFunctions = require("app.utilities.GridIndexFunctions")
 
 --------------------------------------------------------------------------------
--- The functions for MovePath.
+-- The util functions.
 --------------------------------------------------------------------------------
-local function hasGridInMovePath(path, gridIndex)
-    for i, pathItem in ipairs(path) do
-        if (GridIndexFunctions.isEqual(gridIndex, pathItem.gridIndex)) then
-            return true
-        end
-    end
-
-    return false
-end
-
-local function canMovePathExtendToGrid(path, gridIndex)
-    return (not hasGridInMovePath(path, gridIndex)) and (GridIndexFunctions.isAdjacent(path[#path].gridIndex, gridIndex))
-end
-
---------------------------------------------------------------------------------
--- The callback functions on EvtPlayerSelectedGrid.
---------------------------------------------------------------------------------
-local function canUnitTakeAction(model, unitModel)
-    return (unitModel) and (unitModel:getPlayerIndex() == model.m_PlayerIndex) and (unitModel:getState() == "idle")
+local function getMaxRange(unitModel)
+    return math.min(unitModel:getMovementRange(), unitModel:getCurrentFuel())
 end
 
 local function getRangeConsumption(gridIndex, unitModel, unitMapModel, tileMapModel)
@@ -42,6 +25,104 @@ local function getRangeConsumption(gridIndex, unitModel, unitMapModel, tileMapMo
     return tileActor:getModel():getMoveCostWithMoveType(unitModel:getMovementType())
 end
 
+local function getReachableGrid(grids, gridIndex)
+    if (grids and gridIndex and grids[gridIndex.x]) then
+        return grids[gridIndex.x][gridIndex.y]
+    else
+        return nil
+    end
+end
+
+local function canUnitTakeAction(model, unitModel)
+    return (unitModel) and (unitModel:getPlayerIndex() == model.m_PlayerIndex) and (unitModel:getState() == "idle")
+end
+
+--------------------------------------------------------------------------------
+-- The functions for MovePath.
+--------------------------------------------------------------------------------
+local function hasGridInMovePath(path, gridIndex)
+    for i, pathItem in ipairs(path) do
+        if (GridIndexFunctions.isEqual(gridIndex, pathItem.gridIndex)) then
+            return true, i
+        end
+    end
+
+    return false
+end
+
+local function truncateMovePathToGrid(path, gridIndex)
+    local hasGrid, index = hasGridInMovePath(path, gridIndex)
+    if (hasGrid) then
+        for i = index + 1, #path do
+            path[i] = nil
+        end
+
+        return true
+    else
+        return false
+    end
+end
+
+local function extendMovePathToGrid(path, gridIndex, nextRangeConsumption, maxRange)
+    local length = #path
+    local rangeConsumption = path[length].rangeConsumption + nextRangeConsumption
+
+    if (hasGridInMovePath(path, gridIndex)) or
+       (not GridIndexFunctions.isAdjacent(path[length].gridIndex, gridIndex)) or
+       (rangeConsumption > maxRange) then
+        return false
+    else
+        path[length + 1] = {
+            gridIndex        = gridIndex,
+            rangeConsumption = rangeConsumption
+        }
+
+        return true
+    end
+end
+
+local function createShortestMovePathToGrid(gridIndex, reachableGrids)
+    local gridItem = getReachableGrid(reachableGrids, gridIndex)
+    assert(gridItem, "ModelActionPlanner-createShortestMovePathToGrid() the grid is not reachable.")
+
+    local reversePath = {}
+    local x, y = gridIndex.x, gridIndex.y
+    while (gridItem) do
+        reversePath[#reversePath + 1] = {
+            gridIndex        = {x = x, y = y},
+            rangeConsumption = gridItem.rangeConsumption
+        }
+
+        if (not gridItem.prevGridIndex) then
+            gridItem = nil
+        else
+            x, y = gridItem.prevGridIndex.x, gridItem.prevGridIndex.y
+            gridItem = getReachableGrid(reachableGrids, gridItem.prevGridIndex)
+        end
+    end
+
+    local path, length = {}, #reversePath
+    for i = 1, length do
+        path[i] = reversePath[length - i + 1]
+    end
+
+    return path
+end
+
+local function updateMovePathWithDestinationGrid(gridIndex, model)
+    local focusUnitModel       = model.m_FocusUnitActor:getModel()
+    local maxRange             = getMaxRange(focusUnitModel)
+    local nextRangeConsumption = getRangeConsumption(gridIndex, focusUnitModel, model.m_UnitMapModel, model.m_TileMapModel)
+
+    if (not truncateMovePathToGrid(model.m_MovePath, gridIndex)) and
+       (not extendMovePathToGrid(model.m_MovePath, gridIndex, nextRangeConsumption, maxRange)) then
+        model.m_MovePath = createShortestMovePathToGrid(gridIndex, model.m_ReachableGrids)
+    end
+end
+
+--------------------------------------------------------------------------------
+-- The callback functions on EvtPlayerSelectedGrid.
+--------------------------------------------------------------------------------
 local function updateReachableGrids(grids, gridIndex, prevGridIndex, rangeConsumption)
     local x, y = gridIndex.x, gridIndex.y
     grids[x] = grids[x] or {}
@@ -58,14 +139,6 @@ local function updateReachableGrids(grids, gridIndex, prevGridIndex, rangeConsum
     end
 end
 
-local function getReachableGrid(grids, gridIndex)
-    if (grids and grids[gridIndex.x]) then
-        return grids[gridIndex.x][gridIndex.y]
-    else
-        return nil
-    end
-end
-
 local function pushBackToAvailableGridList(list, gridIndex, prevGridIndex, rangeConsumption)
     list[#list + 1] = {
         gridIndex        = gridIndex,
@@ -75,7 +148,7 @@ local function pushBackToAvailableGridList(list, gridIndex, prevGridIndex, range
 end
 
 local function getReachableGridsForUnit(unitModel, unitMapModel, tileMapModel)
-    local maxRange = math.min(unitModel:getMovementRange(), unitModel:getCurrentFuel())
+    local maxRange = getMaxRange(unitModel)
     local reachableGrids, availableGridList = {}, {}
     pushBackToAvailableGridList(availableGridList, unitModel:getGridIndex(), nil, 0)
 
@@ -107,6 +180,10 @@ local function onEvtPlayerSelectedGrid(model, gridIndex)
             model.m_State          = "makingMovePath"
             model.m_FocusUnitActor = unit
             model.m_ReachableGrids = getReachableGridsForUnit(unit:getModel(), model.m_UnitMapModel, model.m_TileMapModel)
+            model.m_MovePath       = {{
+                gridIndex        = unit:getModel():getGridIndex(),
+                rangeConsumption = 0
+            }}
 
             if (model.m_View) then
                 model.m_View:showReachableGrids(model.m_ReachableGrids)
@@ -120,6 +197,8 @@ local function onEvtPlayerSelectedGrid(model, gridIndex)
                 model.m_View:hideReachableGrids()
             end
         else
+            updateMovePathWithDestinationGrid(gridIndex, model)
+
             print("touched a reachable grid.")
         end
     end
