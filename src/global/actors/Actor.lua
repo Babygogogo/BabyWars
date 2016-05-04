063a3383b59ddfcef311fbbf7e770d0f3c39163c
Babygogogo <101599838@qq.com>
2016-05-04 13:31:52 +0800

    Add comments for global files.

--- src/global/actors/Actor.lua
+++ src/global/actors/Actor.lua
@@ -1,4 +1,39 @@
 
+--[[--------------------------------------------------------------------------------
+-- Actor是游戏中几乎所有元素的统一的容器，能够各容纳最多一个model和view。
+--
+-- 主要职责和使用场景举例：
+--   创建新的actor实例（此功能可以考虑改由ActorManager实现）
+--   提供接口给外界访问actor内的model和view
+--   接收node事件（下面有详细描述）
+--
+-- 其他：
+--   - actor实例举例：
+--     刚进游戏所看到的主场景是一个actor实例，它所包含的model和view分别是ModelSceneMain和ViewSceneMain类的实例。
+--     主场景中的战局列表也是一个actor实例，它所包含的model和view分别是ModelWarList和ViewWarList类的实例。
+--
+--   - actor间的树状层级关系：
+--     继续以主场景和战局列表为例。在逻辑上，可以认为主场景是父节点，战局列表是子节点（并非继承关系，而是类似于cocos2d-x中的父显示节点和子显示节点的关系）。
+--     但actor本身并没有体现父子关系的代码（这是因为一个父actor可能有很多作用迥异的子actor，强行在actor里写通用的处理函数没有太多意义），因此要手动在model里添加相应代码。
+--     也就是说，以主场景和战局列表为例，我们需要在ModelSceneMain里创建和维护战局列表的actor。
+--     同时，为了使得战局列表能够显示出来，我们还需要调用cc.Node:addChild()，把战局列表的view添加到主场景的view中（具体参看ViewWarList）。
+--
+--     场景的actor是最顶级的父节点，也就是根节点，因此又称为rootActor。
+--     rootActor的view必须继承cc.Scene，而为了显示它，需要通过ActorManager的相关函数（参看ActorManager）。
+--
+--   - node事件是什么
+--     node事件是由引擎内部在显示节点发生特定情况时派发的事件。这些特定情况包括：节点进入场景，节点离开场景（被清理），等等。
+--     目前actor之所以要接收node事件，是为了在节点进入和离开节点时，为其注册/反注册到场景的EventDispatcher中。
+--     这增大了actor的复杂度以及和引擎的耦合度，因此可以考虑改用别的方式注册/反注册EventDispatcher，并把actor接收node事件的功能删掉。
+--
+--   - model和view，以及游戏引擎的关系
+--     虽然actor本身并没有强制要求，但一般来说，特定的model总是配合特定的view来一起工作的。这些有特定联系的model和view，类名也基本是一致的（分别是ModelXXX和ViewXXX）。
+--     一般来说，特定的view的公有接口，应该只由相对应的model来调用。
+--
+--     actor和model都可以独立于游戏引擎存在，但view则不能（因为显示画面需要依靠引擎）。
+--     理想状态下，如果需要更换引擎，那么我们只需要改写view就可以，其他都可以不用更改。
+--]]--------------------------------------------------------------------------------
+
 local Actor = class("Actor")
 
 local ComponentManager = require("global.components.ComponentManager")
--- src/global/components/ComponentManager.lua
+++ src/global/components/ComponentManager.lua
@@ -1,4 +1,29 @@
 
+--[[--------------------------------------------------------------------------------
+-- ComponentManager用于把component绑定到宿主上，以及提供相关的访问接口。
+--
+-- 主要职责：
+--   绑定/解绑component
+--   提供接口给外界访问component
+--
+-- 使用场景举例：
+--   ModelTile和ModelUnit在初始化时，都会使用ComponentManager绑定相应的component。
+--
+-- 其他：
+--   - 绑定component时，会占用宿主的component_域。因此宿主不应自行使用这个域。
+--
+--   - 绑定component时，不仅会把component的实例加入到component_域中，还会把component指定的方法直接绑定到宿主上。举例：
+--     target = {}
+--     ComponentExample = { EXPORT_METHOD = "someMethod", ...}
+--     ComponentManager.bindComponent(target, ComponentExample)
+--     这三句代码执行后，target就会自动具有一个名为someMethod的域（另外还有component_域，里面存放了一个ComponentExample的实例）。
+--     之后，其他代码就可以直接调用target.someMethod，就好像target本来就具有这个域。
+--
+--   - ModelTile和ModelUnit都是component的重度使用者。可以通过更改GameConstant下的设定，改变它们所绑定的component，这也将直接影响它们的行为。
+--
+--   - 允许在任意时刻绑定/解绑component，但为了不增加理解难度，一般只在初始化时进行绑定或解绑。
+--]]--------------------------------------------------------------------------------
+
 local ComponentManager = {}
 
 --------------------------------------------------------------------------------
