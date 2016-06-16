139bb5264f5c27af814549ee0a1789c56fdc3f25
Babygogogo <101599838@qq.com>
2016-06-16 23:32:19 +0800

    Enable to hide some items for WarCommandMenu.

--- src/app/views/sceneWar/ViewWarCommandMenu.lua
+++ src/app/views/sceneWar/ViewWarCommandMenu.lua
@@ -1,22 +1,24 @@
 
 local ViewWarCommandMenu = class("ViewWarCommandMenu", cc.Node)
 
-local MENU_BACKGROUND_WIDTH  = 250
-local MENU_BACKGROUND_HEIGHT = display.height * 0.8
-local MENU_BACKGROUND_POSITION_X = (display.width - MENU_BACKGROUND_WIDTH) / 2
-local MENU_BACKGROUND_POSITION_Y = (display.height - MENU_BACKGROUND_HEIGHT) / 2
+local BACKGROUND_WIDTH     = 250
+local BACKGROUND_HEIGHT    = display.height * 0.8
+local BACKGROUND_POS_X     = (display.width - BACKGROUND_WIDTH) / 2
+local BACKGROUND_POS_Y     = (display.height - BACKGROUND_HEIGHT) / 2
+local BACKGROUND_CAPINSETS = {x = 4, y = 6, width = 1, height = 1}
 
-local LIST_WIDTH  = MENU_BACKGROUND_WIDTH - 10
-local LIST_HEIGHT = MENU_BACKGROUND_HEIGHT - 14
-local LIST_POSITION_X = MENU_BACKGROUND_POSITION_X + 5
-local LIST_POSITION_Y = MENU_BACKGROUND_POSITION_Y + 6
+local LIST_VIEW_WIDTH        = BACKGROUND_WIDTH - 10
+local LIST_VIEW_HEIGHT       = BACKGROUND_HEIGHT - 14
+local LIST_VIEW_POS_X        = BACKGROUND_POS_X + 10
+local LIST_VIEW_POS_Y        = BACKGROUND_POS_Y + 6
+local LIST_VIEW_ITEMS_MARGIN = 10
 
-local ITEM_WIDTH              = MENU_BACKGROUND_WIDTH - 20
-local ITEM_HEIGHT             = 45
-local ITEM_CAPINSETS          = {x = 1, y = ITEM_HEIGHT, width = 1, height = 1}
-local ITEM_FONT_COLOR         = {r = 255, g = 255, b = 255}
-local ITEM_FONT_OUTLINE_COLOR = {r = 0,   g = 0,   b = 0}
-local ITEM_FONT_OUTLINE_WIDTH = 2
+local ITEM_WIDTH         = BACKGROUND_WIDTH - 20
+local ITEM_HEIGHT        = 45
+local ITEM_CAPINSETS     = {x = 1, y = ITEM_HEIGHT, width = 1, height = 1}
+local ITEM_FONT_COLOR    = {r = 255, g = 255, b = 255}
+local ITEM_OUTLINE_COLOR = {r = 0,   g = 0,   b = 0}
+local ITEM_OUTLINE_WIDTH = 2
 
 --------------------------------------------------------------------------------
 -- The util functions.
@@ -36,7 +38,7 @@ local function createViewItem(item)
         :setTitleColor(ITEM_FONT_COLOR)
         :setTitleText(item.name)
 
-    view:getTitleRenderer():enableOutline(ITEM_FONT_OUTLINE_COLOR, ITEM_FONT_OUTLINE_WIDTH)
+    view:getTitleRenderer():enableOutline(ITEM_OUTLINE_COLOR, ITEM_OUTLINE_WIDTH)
 
     view:addTouchEventListener(function(sender, eventType)
         if (eventType == ccui.TouchEventType.ended) then
@@ -48,65 +50,44 @@ local function createViewItem(item)
 end
 
 --------------------------------------------------------------------------------
--- The screen background (the transparent grey mask).
+-- The composition elements.
 --------------------------------------------------------------------------------
-local function createScreenBackground()
-    local background = cc.LayerColor:create({r = 0, g = 0, b = 0, a = 160})
-    background:setContentSize(display.width, display.height)
+local function initGreyMask(self)
+    local greyMask = cc.LayerColor:create({r = 0, g = 0, b = 0, a = 160})
+    greyMask:setContentSize(display.width, display.height)
         :ignoreAnchorPointForPosition(true)
 
-    return background
+    self.m_GreyMask = greyMask
+    self:addChild(greyMask)
 end
 
-local function initWithScreenBackground(self, background)
-    self.m_ScreenBackground = background
-    self:addChild(background)
-end
-
---------------------------------------------------------------------------------
--- The menu background.
---------------------------------------------------------------------------------
-local function createMenuBackground()
-    local background = cc.Scale9Sprite:createWithSpriteFrameName("c03_t01_s01_f01.png", {x = 4, y = 6, width = 1, height = 1})
+local function initBackground(self)
+    local background = cc.Scale9Sprite:createWithSpriteFrameName("c03_t01_s01_f01.png", BACKGROUND_CAPINSETS)
     background:ignoreAnchorPointForPosition(true)
-        :setPosition(MENU_BACKGROUND_POSITION_X, MENU_BACKGROUND_POSITION_Y)
+        :setPosition(BACKGROUND_POS_X, BACKGROUND_POS_Y)
 
-        :setContentSize(MENU_BACKGROUND_WIDTH, MENU_BACKGROUND_HEIGHT)
+        :setContentSize(BACKGROUND_WIDTH, BACKGROUND_HEIGHT)
 
-    return background
-end
+        :setOpacity(180)
 
-local function initWithMenuBackground(self, background)
     self.m_MenuBackground = background
     self:addChild(background)
 end
 
---------------------------------------------------------------------------------
--- The list view.
---------------------------------------------------------------------------------
-local function createListView()
+local function initListView(self)
     local listView = ccui.ListView:create()
     listView:ignoreAnchorPointForPosition(true)
-        :setPosition(LIST_POSITION_X, LIST_POSITION_Y)
+        :setPosition(LIST_VIEW_POS_X, LIST_VIEW_POS_Y)
 
-        :setContentSize(LIST_WIDTH, LIST_HEIGHT)
+        :setContentSize(LIST_VIEW_WIDTH, LIST_VIEW_HEIGHT)
 
-        :setItemsMargin(20)
-        :setGravity(ccui.ListViewGravity.centerHorizontal)
-        :setCascadeOpacityEnabled(true)
+        :setItemsMargin(LIST_VIEW_ITEMS_MARGIN)
 
-    return listView
-end
-
-local function initWithListView(self, listView)
     self.m_ListView = listView
     self:addChild(listView)
 end
 
---------------------------------------------------------------------------------
--- The touch listener.
---------------------------------------------------------------------------------
-local function createTouchListener(self)
+local function initTouchListener(self)
     local touchListener = cc.EventListenerTouchOneByOne:create()
     touchListener:setSwallowTouches(true)
 
@@ -118,10 +99,6 @@ local function createTouchListener(self)
         self:setEnabled(false)
     end, cc.Handler.EVENT_TOUCH_ENDED)
 
-    return touchListener
-end
-
-local function initWithTouchListener(self, touchListener)
     self.m_TouchListener = touchListener
     self:getEventDispatcher():addEventListenerWithSceneGraphPriority(self.m_TouchListener, self)
 end
@@ -130,15 +107,12 @@ end
 -- The constructor and initializers.
 --------------------------------------------------------------------------------
 function ViewWarCommandMenu:ctor(param)
-    initWithScreenBackground(self, createScreenBackground())
-    initWithMenuBackground(  self, createMenuBackground())
-    initWithListView(        self, createListView())
-    initWithTouchListener(   self, createTouchListener(self))
+    initGreyMask(     self)
+    initBackground(   self)
+    initListView(     self)
+    initTouchListener(self)
 
     self:ignoreAnchorPointForPosition(true)
-
-        :setCascadeOpacityEnabled(true)
-        :setOpacity(200)
 
     return self
 end
@@ -154,6 +128,16 @@ end
 
 function ViewWarCommandMenu:removeAllItems()
     self.m_ListView:removeAllItems()
+
+    return self
+end
+
+function ViewWarCommandMenu:setItems(items)
+    self:removeAllItems()
+
+    for _, item in ipairs(items) do
+        self:createAndPushBackViewItem(item)
+    end
 
     return self
 end
