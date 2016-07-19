7aad6cc9cb0a9f86c290512c37e3cddbd875e7cf
Babygogogo <101599838@qq.com>
2016-07-19 15:46:14 +0800

    Enable to supply units at the beginning of a turn.

--- src/app/components/UnitSupplier.lua
+++ src/app/components/UnitSupplier.lua
@@ -1,7 +1,8 @@
 
 local UnitSupplier = require("src.global.functions.class")("UnitSupplier")
 
-local ComponentManager = require("src.global.components.ComponentManager")
+local ComponentManager   = require("src.global.components.ComponentManager")
+local GridIndexFunctions = require("src.app.utilities.GridIndexFunctions")
 
 local EXPORTED_METHODS = {
     "canSupplyModelUnit",
@@ -21,6 +22,32 @@ local function canSupplyAmmo(modelUnit)
         (modelUnit:getPrimaryWeaponCurrentAmmo() < modelUnit:getPrimaryWeaponMaxAmmo())
 end
 
+local function supplyModelUnit(modelUnit)
+    if (canSupplyFuel(modelUnit)) then
+        modelUnit:setCurrentFuel(modelUnit:getMaxFuel())
+    end
+    if (canSupplyAmmo(modelUnit)) then
+        modelUnit:setPrimaryWeaponCurrentAmmo(modelUnit:getPrimaryWeaponMaxAmmo())
+    end
+end
+
+--------------------------------------------------------------------------------
+-- The private callback functions on script events.
+--------------------------------------------------------------------------------
+local function onEvtTurnPhaseSupplyUnit(self, event)
+    local modelUnitMap = event.modelUnitMap
+    local supplier     = self.m_Owner
+    if ((supplier:getPlayerIndex() == event.playerIndex)                       and
+        (not modelUnitMap:getLoadedModelUnitWithUnitId(supplier:getUnitId()))) then
+        for _, gridIndex in pairs(GridIndexFunctions.getAdjacentGrids(supplier:getGridIndex(), modelUnitMap:getMapSize())) do
+            local target = modelUnitMap:getModelUnit(gridIndex)
+            if ((target) and (self:canSupplyModelUnit(target))) then
+                supplyModelUnit(target)
+            end
+        end
+    end
+end
+
 --------------------------------------------------------------------------------
 -- The constructor and initializers.
 --------------------------------------------------------------------------------
@@ -33,6 +60,24 @@ function UnitSupplier:loadTemplate(template)
 end
 
 function UnitSupplier:loadInstantialData(data)
+    return self
+end
+
+function UnitSupplier:setRootScriptEventDispatcher(dispatcher)
+    assert(self.m_RootScriptEventDispatcher == nil, "UnitSupplier:setRootScriptEventDispatcher() the dispatcher has been set.")
+
+    self.m_RootScriptEventDispatcher = dispatcher
+    dispatcher:addEventListener("EvtTurnPhaseSupplyUnit", self)
+
+    return self
+end
+
+function UnitSupplier:unsetRootScriptEventDispatcher()
+    assert(self.m_RootScriptEventDispatcher, "UnitSupplier:unsetRootScriptEventDispatcher() the dispatcher hasn't been set.")
+
+    self.m_RootScriptEventDispatcher:removeEventListener("EvtTurnPhaseSupplyUnit", self)
+    self.m_RootScriptEventDispatcher = nil
+
     return self
 end
 
@@ -58,16 +103,22 @@ function UnitSupplier:onUnbind()
 end
 
 --------------------------------------------------------------------------------
+-- The callback functions on script events.
+--------------------------------------------------------------------------------
+function UnitSupplier:onEvent(event)
+    if (event.name == "EvtTurnPhaseSupplyUnit") then
+        onEvtTurnPhaseSupplyUnit(self, event)
+    end
+
+    return self
+end
+
+--------------------------------------------------------------------------------
 -- The functions for doing actions.
 --------------------------------------------------------------------------------
 function UnitSupplier:doActionSupplyModelUnit(action, targetModelUnits)
     for _, modelUnit in pairs(targetModelUnits) do
-        if (canSupplyFuel(modelUnit)) then
-            modelUnit:setCurrentFuel(modelUnit:getMaxFuel())
-        end
-        if (canSupplyAmmo(modelUnit)) then
-            modelUnit:setPrimaryWeaponCurrentAmmo(modelUnit:getPrimaryWeaponMaxAmmo())
-        end
+        supplyModelUnit(modelUnit)
     end
 
     return self.m_Owner
