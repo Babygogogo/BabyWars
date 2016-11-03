a2c70582cb279641268e5c69c8b161746091b2c2
Babygogogo <101599838@qq.com>
2016-11-04 00:39:48 +0800

    [Modify] Refactor VisibilityFunctions.

--- src/app/utilities/VisibilityFunctions.lua
+++ src/app/utilities/VisibilityFunctions.lua
@@ -36,24 +36,6 @@ end
 --------------------------------------------------------------------------------
 -- The public functions.
 --------------------------------------------------------------------------------
-function VisibilityFunctions.isModelUnitVisibleToPlayerIndex(modelUnit, sceneWarFileName, playerIndex)
-    -- TODO: deal with fog of war.
-    if ((modelUnit:getPlayerIndex() == playerIndex) or
-        (not isModelUnitDiving(modelUnit)))         then
-        return true
-    else
-        local modelUnitMap = getModelUnitMap(sceneWarFileName)
-        for _, adjacentGridIndex in ipairs(getAdjacentGrids(modelUnit:getGridIndex(), modelUnitMap:getMapSize())) do
-            local adjacentModelUnit = modelUnitMap:getModelUnit(adjacentGridIndex)
-            if ((adjacentModelUnit) and (adjacentModelUnit:getPlayerIndex() == playerIndex)) then
-                return true
-            end
-        end
-
-        return false
-    end
-end
-
 function VisibilityFunctions.isUnitOnMapVisibleToPlayerIndex(sceneWarFileName, gridIndex, isDiving, unitPlayerIndex, opponentPlayerIndex)
     assert(type(unitPlayerIndex)     == "number", "VisibilityFunctions.isUnitOnMapVisibleToPlayerIndex() invalid unitPlayerIndex: " .. (unitPlayerIndex or ""))
     assert(type(opponentPlayerIndex) == "number", "VisibilityFunctions.isUnitOnMapVisibleToPlayerIndex() invalid opponentPlayerIndex: " .. (opponentPlayerIndex or ""))
